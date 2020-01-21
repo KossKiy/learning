9f1f99c4f53f01960ca927f9417b53c822ce20a6
KossKiy <kosforever@gmail.com>
2020-01-21 17:33:18 +0200

    Добавил проверку на пустоту поля

--- index.php
+++ index.php
@@ -12,19 +12,26 @@ $email = $_POST['email'];
 <body>
 <form method="post" action="../learning/index.php">
     <label>Name</label>
-    <input type="text" name="name">
+    <input type="text" name="name" value="<?= $_POST['name'];?>"><br /><br />
     <label>SurName</label>
-    <input type="text" name="surname">
+    <input type="text" name="surname" value="<?= $_POST['surname'];?>"><br /><br />
     <label>Email</label>
-    <input type="email" name="surname">
+    <input type="email" name="email" value="<?= $_POST['email'];?>"><br /><br />
     <input type="submit" name="done">
 
 </form>
 </body>
 </html>
 
+
 <?php
-if (isset($_POST['done']) && $name != "" && $surname != "" && $email != "") {
-    echo "Hello, " . $name . " " . $surname . "<br />" . "Your email is:" . $email;
+if (isset($_POST['done']) && !empty($name) && !empty($surname) && !empty($email)) {
+    echo "Hello, " . $name . " " . $surname . "<br />" . "Your email is: " . $email;
+} else {
+    foreach ($_POST as $key => $value) {
+        if ($value == "") {
+            echo "<p style=\"color:red\">You need to fill <b>$key</b> field</p>";
+        }
+    }
 }
 ?>

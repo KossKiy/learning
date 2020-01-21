d197516fb6dcbc7146377078ca25a447fbb6c3e6
KossKiy <kosforever@gmail.com>
2020-01-21 16:20:51 +0200

    Перенес все в один файл

--- index.php
+++ index.php
@@ -1,5 +1,25 @@
 <?php
 $name = $_POST['name'];
 $surname = $_POST['surname'];
-echo "Hello, ".$name." ".$surname;
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Test hello form</title>
+</head>
+<body>
+<form method="post" action="../learning/index.php">
+    <label>Name</label>    <input type="text" name="name">
+    <label>SurName</label>
+    <input type="text" name="surname">
+    <input type="submit" name="done">
+</form>
+</body>
+</html>
 
+<?php
+if (isset($_POST['done'])&& $name !="" ){
+echo "Hello, ".$name." ".$surname;
+}
+?>
